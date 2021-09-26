a496930ca4d1af9e2b12c85ad7945014a7195ce5
Diana162002 <shalilowa2002@yandex.ru>
2021-09-26 14:04:59 +0300

    first commit

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer className="page-footer light-green darken-3">
+            <div className="container">
+                /Prod by D. © {new Date().getFullYear()} All rights reserved
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
